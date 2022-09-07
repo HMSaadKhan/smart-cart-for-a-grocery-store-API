cba59e0e0a33681a90f1e07a4e88b29227cf634a
Saad_Khan <h.saadkhan501@gmail.com>
2022-09-07 14:45:20 +0500

    category routes

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,7 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+var { Category } = require("../../models/categories");
 
-/* GET home page. */
+// const createCategory = async (name) => {
+//   let category = new CategoryModel();
+//   category.name = name;
+//   await category.save();
+// };
 
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    let category = new Category();
+    category._id = new mongoose.Types.ObjectId();
+    category.name = req.body.name;
+    await category.save();
+    return res.send("Product Added");
+  } catch (err) {
+    console.log(err);
+    return res.send("Product not Added");
+  }
+});
 
+router.get("/", async (req, res) => {
+  try {
+    let category = await Category.find();
+    return res.send(category);
+  } catch (err) {
+    console.log(err);
+    return res.send("Product not Added");
+  }
+});
 module.exports = router;
